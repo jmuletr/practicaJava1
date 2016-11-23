f66cca66e409eb63b681290a4425d0acf55239b8
jmuletr <jmuletr@gmail.com>
2016-11-23 19:40:43 +0100

    vigenere ok

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -34,7 +34,34 @@ public class Vigenere {
     }
 
     static String decode(String s, String password) {
-        return null;
+        s = s.toUpperCase();
+        password = password.toUpperCase();
+        StringBuilder s2 = new StringBuilder();
+        s = normalitzar(s);
+        password = normalitzar(password);
+        int cont = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 0 && s.charAt(i) < 65 || s.charAt(i) > 90){
+                s2.append(s.charAt(i));
+            }
+            if (s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                if (s.charAt(i) - (password.charAt(cont) - 64) < 65){
+                    char ch = s.charAt(i);
+                    char ch2 = (char)((int)ch + 26 - (password.charAt(cont) - 64));
+                    s2.append(ch2);
+                    cont++;
+                } else {
+                    char ch = s.charAt(i);
+                    char ch2 = (char)((int)ch - (password.charAt(cont) - 64));
+                    s2.append(ch2);
+                    cont++;
+                }
+            }
+            if (cont == password.length()){
+                cont = 0;
+            }
+        }
+        return s2.toString();
     }
 
     public static String normalitzar(String input) {
