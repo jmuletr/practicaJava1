b3a7766765cb647cd1d80e56ed35e0a90b2a0893
jmuletr <jmuletr@gmail.com>
2016-11-23 14:30:01 +0100

    test 2 cesar ok

--- src/Caesar.java
+++ src/Caesar.java
@@ -26,7 +26,28 @@ public class Caesar {
     }
 
     static String decypher(String s, int delta) {
-        return null;
+        s = s.toUpperCase();
+        StringBuilder s2 = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (delta > 26){
+                delta = delta % 26;
+            }
+            if (s.charAt(i) >= 0 && s.charAt(i) < 65 || s.charAt(i) > 90){
+                s2.append(s.charAt(i));
+            }
+            if (s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                if (s.charAt(i) - delta < 65){
+                    char ch = s.charAt(i);
+                    char ch2 = (char)((int)ch - delta + 26);
+                    s2.append(ch2);
+                } else {
+                    char ch = s.charAt(i);
+                    char ch2 = (char)((int)ch - delta);
+                    s2.append(ch2);
+                }
+            }
+        }
+        return s2.toString();
     }
 
     static String magic(String s) {
