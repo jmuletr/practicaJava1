335d93486a05995c10af2a9e2610cead76f82cbf
jmuletr <jmuletr@gmail.com>
2016-11-29 18:44:39 +0100

    optimitzant cesar

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,66 +1,69 @@
 
 public class Caesar {
     static String cypher(String s, int delta) {
-        //Es pasa el String a majuscules
-        s = s.toUpperCase();
-        //Variable s2(string2) on es generara el missatge codificat
-        StringBuilder s2 = new StringBuilder();
-        //Bucle per fer que delta no sigui major que 26 i es pugui treballar amb ell
-        for (int i = 0; i < s.length(); i++) {
-            if (delta > 26) {
-                delta = delta % 26;
-            }
-            //Comprovacio de si el caracter no es una lletra majuscula
-            if (s.charAt(i) >= 0 && s.charAt(i) < 65 || s.charAt(i) > 90) {
-                s2.append(s.charAt(i));
-            }
-            //Comprovacio si el caracter es una lletra majuscula
-            if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
-                /**Si el caracter mes delta es major a 90(Z) es guarda el caracter a ch(character)
-                 i a ch2(character2) es guarda el caracter mes delta i se li resta 26 per a que el valor minim sigui 65(A),
-                 i despres s'afelleix el caracter ch2 a la variable s2.**/
-                if (s.charAt(i) + delta > 90) {
-                    char ch = s.charAt(i);
-                    char ch2 = (char) ((int) ch + delta - 26);
-                    s2.append(ch2);
-                    //Si el valor del caracter mes delta no pasa de 90(Z) s'afegeix ch2 directament a s2.
-                } else {
-                    char ch = s.charAt(i);
-                    char ch2 = (char) ((int) ch + delta);
-                    s2.append(ch2);
-                }
-            }
-        }
-        //En acabar retorna el valor de s2 com a String.
-        return s2.toString();
+        s = work(s, delta, true);
+        //En acabar retorna s encriptat.
+        return s;
     }
 
     static String decypher(String s, int delta) {
+        s = work(s, delta, false);
+        //En acabar retorna s desencriptat.
+        return s;
+    }
+
+    static String work(String s, int key, boolean op) {
+        //Es pasa el String a majuscules
         s = s.toUpperCase();
+        //Variable s2(string2) on es generara el missatge tractat.
         StringBuilder s2 = new StringBuilder();
+        //Bucle per treballar amb tot el string.
         for (int i = 0; i < s.length(); i++) {
-            if (delta > 26) {
-                delta = delta % 26;
+            //fer que delta no sigui major que 26 i es pugui treballar amb ell.
+            if (key > 26) {  //TODO fer que accepti delta negatiu.
+                key = key % 26;
             }
-            if (s.charAt(i) >= 0 && s.charAt(i) < 65 || s.charAt(i) > 90) {
+            //Comprovacio de si el caracter no es una lletra majuscula
+            if (s.charAt(i) >= 0 && s.charAt(i) < 'A' || s.charAt(i) > 'Z') {
                 s2.append(s.charAt(i));
             }
-            if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
-                /**Per a descifrar el missatge es mira que el caracter menys delta no sigui menor a 65(A) i
-                 si o es se li suma 26 per tornar a 90(Z).**/
-                if (s.charAt(i) - delta < 65) {
-                    char ch = s.charAt(i);
-                    char ch2 = (char) ((int) ch - delta + 26);
-                    s2.append(ch2);
-                    //si el caracter menys delta no es menor a 65(A) se fica directament a s2.
-                } else {
-                    char ch = s.charAt(i);
-                    char ch2 = (char) ((int) ch - delta);
-                    s2.append(ch2);
+            //comprovacio si s'ha de desencriptar o encriptar.
+            if (!op) {
+                //Comprovacio si el caracter es una lletra majuscula
+                if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                    /**Per a descifrar el missatge es mira que el caracter menys delta no sigui menor a 65(A) i
+                     si o es se li suma 26 per tornar a 90(Z).**/
+                    if (s.charAt(i) - key < 'A') {
+                        char ch = s.charAt(i);
+                        char ch2 = (char) ((int) ch - key + 26);
+                        s2.append(ch2);
+                        //si el caracter menys delta no es menor a 65(A) se fica directament a s2.
+                    } else {
+                        char ch = s.charAt(i);
+                        char ch2 = (char) ((int) ch - key);
+                        s2.append(ch2);
+                    }
+                }
+            } else {
+                //Comprovacio si el caracter es una lletra majuscula
+                if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                    /**Si el caracter mes delta es major a 90(Z) es guarda el caracter a ch(character)
+                     i a ch2(character2) es guarda el caracter mes delta i se li resta 26 per a que el valor minim sigui 65(A),
+                     i despres s'afelleix el caracter ch2 a la variable s2.**/
+                    if (s.charAt(i) + key > 'Z') {
+                        char ch = s.charAt(i);
+                        char ch2 = (char) ((int) ch + key - 26);
+                        s2.append(ch2);
+                        //Si el valor del caracter mes delta no pasa de 90(Z) s'afegeix ch2 directament a s2.
+                    } else {
+                        char ch = s.charAt(i);
+                        char ch2 = (char) ((int) ch + key);
+                        s2.append(ch2);
+                    }
                 }
             }
         }
-        //En acabar retorna el valor de s2 com a String.
+        //retorna s2 com a string.
         return s2.toString();
     }
 
